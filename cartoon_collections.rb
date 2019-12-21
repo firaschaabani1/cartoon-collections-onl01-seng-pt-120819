3f8b1f5831ac1c3d1aac0218a2ad0a53438cb69f
Firas Chaabani <firaschaabani1@gmail.com>
2019-12-21 03:08:08 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,20 @@
 def roll_call_dwarves# code an argument here
-  # Your code here
+  names = ["Doc", "Dopey", "Bashful", "Grumpy"]
+  names.each
+  puts (names)#Your code here
 end
 
 def summon_captain_planet# code an argument here
-  # Your code here
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]  # Your code here
+planeteer_calls.map! {|call| call.capitalize}
+planeteer_calls. {|call| puts call + '!'}
 end
 
 def long_planeteer_calls# code an argument here
-  # Your code here
+
+if long_planeteer_calls.lenght <4
+  return "false"
+else return"true"  # Your code here
 end
 
 def find_the_cheese# code an argument here
